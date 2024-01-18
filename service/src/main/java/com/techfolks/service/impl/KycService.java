fb1b1d349f40cfe4287e0e160863f78b7e7d8ab6
Khushee26 <khushisurani.26@gmail.com>
2024-01-19 00:22:26 +0530

    initiate kyc manual and get captcha API

--- service/src/main/java/com/techfolks/service/impl/KycService.java
+++ service/src/main/java/com/techfolks/service/impl/KycService.java
@@ -1,9 +1,17 @@
 package com.techfolks.service.impl;
 
-import com.techfolks.model.request.InitiateKycAuto;
-import com.techfolks.model.response.InitiateKycAutoResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.techfolks.model.request.InitiateKycAuto;
+import com.techfolks.model.request.InitiateKycManual;
+import com.techfolks.model.response.GetCaptchaResponse;
+import com.techfolks.model.response.InitiateKycAutoResponse;
+import com.techfolks.model.response.InitiateKycManualResponse;
 
 public interface KycService {
 	InitiateKycAutoResponse initiateAutoKycFunc(InitiateKycAuto initiateAutoKyc) throws JsonProcessingException;
+
+	InitiateKycManualResponse initiateManualKycFunc(InitiateKycManual initiateManualKyc)
+			throws JsonProcessingException, JsonProcessingException;
+
+	GetCaptchaResponse getCaptchaFunc() throws JsonProcessingException, JsonProcessingException;
 }
--- service/src/main/java/com/techfolks/service/impl/KycServiceImpl.java
+++ service/src/main/java/com/techfolks/service/impl/KycServiceImpl.java
@@ -10,10 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.techfolks.model.request.InitiateKycAuto;
-import com.techfolks.model.response.InitiateKycAutoResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techfolks.model.request.InitiateKycAuto;
+import com.techfolks.model.request.InitiateKycManual;
+import com.techfolks.model.response.GetCaptchaResponse;
+import com.techfolks.model.response.InitiateKycAutoResponse;
+import com.techfolks.model.response.InitiateKycManualResponse;
 
 @Service
 public class KycServiceImpl implements KycService {
@@ -23,17 +26,38 @@ public class KycServiceImpl implements KycService {
 	private String apiUrl = "https://svcdemo.digitap.work/ent/v3/kyc/";
 
 	@Override
-	public InitiateKycAutoResponse initiateAutoKycFunc(InitiateKycAuto initiateAutoKyc) throws JsonProcessingException {
+	public InitiateKycAutoResponse initiateAutoKycFunc(InitiateKycAuto initiateAutoKyc) throws JsonProcessingException, JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         String jsonString = new ObjectMapper().writeValueAsString(initiateAutoKyc);
-        ResponseEntity<String> result = restPostAPICall(apiUrl+"intiate-kyc-auto", jsonString);
+        ResponseEntity<String> result = restAPICall(apiUrl+"intiate-kyc-auto", jsonString, HttpMethod.POST);
         String responseBody = result.getBody();
         InitiateKycAutoResponse jsonObject = new ObjectMapper().readValue(responseBody, InitiateKycAutoResponse.class);
         return jsonObject;
 	}
 	
-	public ResponseEntity<String> restPostAPICall(String url, String data) {
+	@Override
+	public InitiateKycManualResponse initiateManualKycFunc(InitiateKycManual initiateManualKyc) throws JsonProcessingException, JsonProcessingException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String jsonString = new ObjectMapper().writeValueAsString(initiateManualKyc);
+        ResponseEntity<String> result = restAPICall(apiUrl+"intiate-kyc", jsonString, HttpMethod.POST);
+        String responseBody = result.getBody();
+        InitiateKycManualResponse jsonObject = new ObjectMapper().readValue(responseBody, InitiateKycManualResponse.class);
+        return jsonObject;
+	}
+	
+	@Override
+	public GetCaptchaResponse getCaptchaFunc() throws JsonProcessingException, JsonProcessingException {
+		HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        ResponseEntity<String> result = restAPICall(apiUrl+"get-captcha", null, HttpMethod.GET);
+        String responseBody = result.getBody();
+        GetCaptchaResponse jsonObject = new ObjectMapper().readValue(responseBody, GetCaptchaResponse.class);
+        return jsonObject;
+	}
+	
+	public ResponseEntity<String> restAPICall(String url, String data, HttpMethod type) {
 		RestTemplate restTemplate = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -41,7 +65,7 @@ public class KycServiceImpl implements KycService {
         String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
         headers.set(HttpHeaders.AUTHORIZATION, encodedString);
         HttpEntity<String> requestEntity = new HttpEntity<>(data, headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, type, requestEntity, String.class);
         return responseEntity;
 	}
 }
